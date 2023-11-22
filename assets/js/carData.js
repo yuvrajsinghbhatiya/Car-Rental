27add7608124b332e1432e433e395136e82daf80
Yuvraj Singh Bhatiya <yvrjbhatiya>
2023-11-22 13:38:02 +0530

    new

---
+++ assets/js/carData.js
@@ -0,0 +1,29 @@
+const carsData = [
+    {
+      name: 'Mahindra Scorpio N',
+      image: '/assets/img/car1.webp',
+    },
+    {
+      name: 'Tata Punch',
+      image: '/assets/img/car2.webp',
+    },
+    {
+      name: 'Kia Seltos',
+      image: '/assets/img/car3.webp',
+    },
+    {
+      name: 'Hyundai Creta',
+      image: '/assets/img/car4.webp',
+    },
+    {
+      name: 'Mahindra Bolero Neo',
+      image: '/assets/img/car5.webp',
+    },
+    {
+      name: 'Renault Kwid',
+      image: '/assets/img/car6.webp',
+    }
+  ];
+  
+  window.carsData = carsData;
+  
